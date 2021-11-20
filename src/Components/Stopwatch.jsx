09999ff91ad0f8fdf519ce925e93a000afebdf9f
Mangeshi <thisismangeshi@gmail.com>
2021-11-20 17:59:09 +0530

    changed stopwatch ui

--- src/Components/Stopwatch.jsx
+++ src/Components/Stopwatch.jsx
@@ -3,7 +3,6 @@ import "./Stopwatch.css";
 import countdown from "../Images/countdown.png";
 import { Link } from "react-router-dom";
 
-
 const StopWatch = () => {
     const [timer, setTimer] = useState(0);
     const [isPaused, setPause] = useState(false);
@@ -27,48 +26,56 @@ const StopWatch = () => {
         setTimer(0);
     };
 
-    const formatTime = () => {
-
-        const getmilliSeconds = `0${Math.floor(timer / 10)%100}`.slice(-2);
-        const getSeconds = `0${Math.floor(timer / 1000) % 60}`.slice(-2);
-        const getMinutes = `0${Math.floor(timer / 60000) % 60}`.slice(-2);
-        const getHours = `0${Math.floor(timer / 3600000) % 60}`.slice(-2);
-
-        return `${getHours} : ${getMinutes} : ${getSeconds} : ${getmilliSeconds}`;
-    };
-
     return (
         <>
-        <div className="stopwatch">
-
-        <div className="navbar">
-                <ul>
-                    <Link  to="/countdown">
-                        <li>
-                            <img src={countdown} alt="countdown" />
-                        </li>
-                    </Link>
-                </ul>
-            </div>
-        <div className="wrapper">
-            <div className="container">
-                <div className="overlap-box">
-                    <div className="counter">
-                        <h1>{formatTime()}</h1>
+            <div className="stopwatch">
+                <div className="navbar">
+                    <ul>
+                        <Link to="/countdown">
+                            <li>
+                                <img src={countdown} alt="countdown" />
+                            </li>
+                        </Link>
+                    </ul>
+                </div>
+                <div className="wrapper">
+                    <div className="container">
+                        <div className="counter">
+                            <h1 className="count">
+                                {`0${Math.floor(timer / 3600000) % 60}`.slice(
+                                    -2
+                                )}
+                            </h1>
+                            <h1 className="colon">:</h1>
+                            <h1 className="count">
+                                {`0${Math.floor(timer / 60000) % 60}`.slice(-2)}
+                            </h1>
+                            <h1 className="colon">:</h1>
+                            <h1 className="count">
+                                {`0${Math.floor(timer / 1000) % 60}`.slice(-2)}
+                            </h1>
+                            <h1 className="colon">:</h1>
+                            <h1 className="count">
+                                {`0${Math.floor(timer / 10) % 100}`.slice(-2)}
+                            </h1>
+                        </div>
                     </div>
                     <div className="buttons">
                         {!isPaused ? (
-                            <button className="btn" onClick={handleStart}>Start</button>
+                            <button className="btn" onClick={handleStart}>
+                                Start
+                            </button>
                         ) : (
-                            <button className="btn" onClick={handlePause}>Pause</button>
+                            <button className="btn" onClick={handlePause}>
+                                Pause
+                            </button>
                         )}
-                        <button className="btn" onClick={handleReset}>Reset</button>
+                        <button className="btn" onClick={handleReset}>
+                            Reset
+                        </button>
                     </div>
                 </div>
             </div>
-        </div>
-
-        </div>
         </>
     );
 };
